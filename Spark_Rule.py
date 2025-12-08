c38983b32dd756535d5937b350616e4d6ef14804
Mouse2204 <binh.lethanhh@hcmut.edu.vn>
2025-12-08 09:07:48 +0700

    Test

--- Spark_Rule.py
+++ Spark_Rule.py
@@ -155,5 +155,5 @@ query = kafka_stream.writeStream \
     .option("checkpointLocation", "/tmp/spark-norm-checkpoint") \
     .trigger(processingTime="10 seconds") \
     .start()
-
+#s
 query.awaitTermination()
